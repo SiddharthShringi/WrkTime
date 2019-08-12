3c24274cda1e0c112c83d6923d337c56cdbd46d4
Siddharth Shringi <siddharthshringi@gmail.com>
2019-08-12 19:13:09 +0530

    make login view

--- Api/views.py
+++ Api/views.py
@@ -1,14 +1,16 @@
 from rest_framework import status
 from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
-from .serializers import MenteeRegistrationSerializer, MentorRegistrationSerializer
+from .serializers import (
+    MenteeRegistrationSerializer, MentorRegistrationSerializer, LoginSerializer
+)
 
 
 # Registration Views
 
 class MentorRegistrationAPIView(APIView):
     serializer_class = MentorRegistrationSerializer
-
 
     def post(self, request):
         user = request.data.get('user', {})
@@ -19,7 +21,7 @@ class MentorRegistrationAPIView(APIView):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-class MeneerRegistrationAPIView(APIView):
+class MenteerRegistrationAPIView(APIView):
     serializer_class = MenteeRegistrationSerializer
 
     def post(self, request):
@@ -31,4 +33,15 @@ class MeneerRegistrationAPIView(APIView):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
+class LoginAPIView(APIView):
+    permission_classes = (AllowAny,)
+    serializer_class = LoginSerializer
 
+    def post(self, request):
+        user = request.data.get('user', {})
+
+        serializer = self.serializer_class(data=user)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
