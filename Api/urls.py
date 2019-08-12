bee30546eaf0852362b4cc831228a4e60fc9f73b
Siddharth Shringi <siddharthshringi@gmail.com>
2019-08-12 22:36:47 +0530

    make login endpoint

--- Api/urls.py
+++ Api/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import MenteerRegistrationAPIView, MentorRegistrationAPIView
+from .views import (
+    MenteerRegistrationAPIView, MentorRegistrationAPIView, LoginAPIView
+)
 
 app_name = 'api'
 
 urlpatterns = [
     path('registration/mentor', MentorRegistrationAPIView.as_view()),
     path('registration/mentee/', MenteerRegistrationAPIView.as_view()),
+    path('login/', LoginAPIView.as_view()),
 ]
