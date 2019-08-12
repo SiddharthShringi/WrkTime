758b9531bc0ffe813d0c070f33716bb41494ac72
Siddharth Shringi <siddharthshringi@gmail.com>
2019-08-12 16:09:17 +0530

    make registration views for mentor and mentee

--- Api/views.py
+++ Api/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import MenteeRegistrationSerializer, MentorRegistrationSerializer
 
-# Create your views here.
+
+# Registration Views
+
+class MentorRegistrationAPIView(APIView):
+    serializer_class = MentorRegistrationSerializer
+
+
+    def post(self, request):
+        user = request.data.get('user', {})
+        serializer = self.serializer_class(data=user)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class MeneerRegistrationAPIView(APIView):
+    serializer_class = MenteeRegistrationSerializer
+
+    def post(self, request):
+        user = request.data.get('user', {})
+        serializer = self.serializer_class(data=user)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+
