a5cbb9cd7f11f6b0d687f08f93ed320ec92a49e3
Siddharth Shringi <siddharthshringi@gmail.com>
2019-08-12 19:12:05 +0530

    make login serializer

--- Api/serializers.py
+++ Api/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate
 from rest_framework import serializers
 from .models import User
 
@@ -39,4 +40,39 @@ class MenteeRegistrationSerializer(serializers.ModelSerializer):
     
     def create(self, validated_data):
         return User.objects.create_mentee(**validated_data)
+
+
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.CharField(max_length=255)
+    password = serializers.CharField(max_length=128, write_only=True)
+    token = serializers.CharField(max_length=255, read_only=True)
+
+    def validate(self, data):
+        email = data.get('email', None)
+        password = data.get('password', None)
+
+        if email is None:
+            raise serializers.ValidationError('An email address is required to login.')
+
+        if password is None:
+            raise serializers.ValidationError('A password is required to login.')
+
+
+        user = authenticate(username=email, password=password)
+
+        if user is None:
+            raise serializers.ValidationError('A user with this email and password does not exist.')
+
+        if not user.is_active:
+            raise serializers.ValidationError('This user has been deactivated.')
+
+        return {
+            'email': user.email,
+            'password': user.password,
+            'token': user.token
+        }
+
+
+
     
