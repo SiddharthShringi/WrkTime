4dae7c56c02c58d47ea9775ea5f2817231a704b0
Siddharth Shringi <siddharthshringi@gmail.com>
2019-08-14 20:33:16 +0530

    make 'api/tasks/' endpoint

--- Api/urls.py
+++ Api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import (
-    MenteerRegistrationAPIView, MentorRegistrationAPIView, LoginAPIView
+    MenteerRegistrationAPIView, MentorRegistrationAPIView, LoginAPIView, TaskAPIView
 )
 
 app_name = 'api'
@@ -9,4 +9,5 @@ urlpatterns = [
     path('registration/mentor', MentorRegistrationAPIView.as_view()),
     path('registration/mentee/', MenteerRegistrationAPIView.as_view()),
     path('login/', LoginAPIView.as_view()),
+    path('tasks/', TaskAPIView.as_view()),
 ]
