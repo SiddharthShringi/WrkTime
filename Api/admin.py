d1d6094c025856347e49e034c8293a32f6dcb090
Siddharth Shringi <siddharthshringi@gmail.com>
2019-08-14 20:29:55 +0530

    register models to admin

--- Api/admin.py
+++ Api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User, Task, LearningPath, TaskSubmission
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Task)
+admin.site.register(LearningPath)
+admin.site.register(TaskSubmission)
