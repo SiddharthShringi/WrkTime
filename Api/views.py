852f00dc4f5544e70cce646d0c924384a06af2c1
Siddharth Shringi <siddharthshringi@gmail.com>
2019-08-14 20:32:41 +0530

    make TaskAPIView

--- Api/views.py
+++ Api/views.py
@@ -1,10 +1,11 @@
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.views import APIView
 from .serializers import (
-    MenteeRegistrationSerializer, MentorRegistrationSerializer, LoginSerializer
+    MenteeRegistrationSerializer, MentorRegistrationSerializer, LoginSerializer, TaskSerializer
 )
+from .models import Task
 from .renderers import UserJSONRenderer
 
 
@@ -49,3 +50,22 @@ class LoginAPIView(APIView):
         serializer.save()
 
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+
+class TaskAPIView(APIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = TaskSerializer
+    queryset = Task.objects.all()
+
+    def get(self, request):
+        tasks = self.queryset.all()
+        serializer = self.serializer_class(tasks, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        data = request.data
+        serializer = self.serializer_class(data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(mentor=request.user)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
