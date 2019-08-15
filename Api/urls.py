ce4385eb8324c320ca42ec1321026b60f8918761
Siddharth Shringi <siddharthshringi@gmail.com>
2019-08-15 16:51:06 +0530

    add task/id url to endpoints

--- Api/urls.py
+++ Api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import (
-    MenteerRegistrationAPIView, MentorRegistrationAPIView, LoginAPIView, TaskAPIView
+    MenteerRegistrationAPIView, MentorRegistrationAPIView, LoginAPIView, TaskList, TaskDetail
 )
 
 app_name = 'api'
@@ -9,5 +9,6 @@ urlpatterns = [
     path('registration/mentor', MentorRegistrationAPIView.as_view()),
     path('registration/mentee/', MenteerRegistrationAPIView.as_view()),
     path('login/', LoginAPIView.as_view()),
-    path('tasks/', TaskAPIView.as_view()),
+    path('tasks/', TaskList.as_view()),
+    path('task/<int:pk>/', TaskDetail.as_view()),
 ]
